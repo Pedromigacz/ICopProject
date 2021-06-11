0f5a1e382348fd395d3de4868219d6ce88f27207
Pedro H G Migacz <pedromigacz@gmail.com>
2021-06-11 00:46:20 -0300

    add 404 error handler

--- src/main.js
+++ src/main.js
@@ -1,19 +1,24 @@
-const express = require("express")
-const app = express()
-const mongoose = require('mongoose')
-const mongoData = require('./utils/databaseCredentials.js')
+const express = require("express"),
+      app = express(),
+      mongoose = require('mongoose'),
+      mongoData = require('./utils/databaseCredentials.js')
 
 // Connect to db
 mongoose.connect(`mongodb+srv://${mongoData.name}:${mongoData.password}${mongoData.path}`, {useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection;
 
 // Middlewares
-const { helmet } = require('./middlewares')
+const {
+    helmet
+} = require('./middlewares')
+
 app.use(helmet())
 
 
 // 404 Error handler
-app.get("*", (req, res) => res.send("Welcome to the error page"));
+app.get('*', (req, res, next) => {
+    res.sendStatus(404)
+})
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {console.log('Database Connected successfully')})
