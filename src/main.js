123a47d2f70a4bde6bacdee55ac2fbc72b6a00ad
Pedro Henrique Guimaraes Migacz <pedg0@protonamil.com>
2021-06-11 00:26:16 -0300

    Connect server to mongoDB

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
-const express = require("express");
-const app = express();
-const middleware = require('./middlewares/middlewares.js')
+const express = require("express")
+const app = express()
+const mongoose = require('mongoose')
+const mongoData = require('./utils/databaseCredentials.js')
 
-app.use(middleware.helmet)
+// Connect to db
+mongoose.connect(`mongodb+srv://${mongoData.name}:${mongoData.password}${mongoData.path}`, {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection;
 
+// Middlewares
+const { helmet } = require('./middlewares')
+app.use(helmet())
+
+
+// 404 Error handler
 app.get("*", (req, res) => res.send("Welcome to the error page"));
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {console.log('Database Connected successfully')})
 
 module.exports = app;
