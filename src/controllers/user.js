8390285a4e9ead49b9edd8c589727402680a29c8
Pedro H G Migacz <pedromigacz@gmail.com>
2021-06-12 14:29:46 -0300

    add the login controller logic

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -22,8 +22,45 @@ exports.register = async (req, res, next) => {
   }
 };
 
-exports.login = (req, res, next) => {
-  res.send("login Route");
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({
+      success: false,
+      error: "Please provide email and password",
+    });
+  }
+
+  try {
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      res.status(404).json({
+        success: false,
+        error: "Invalid credentials",
+      });
+    }
+
+    const isMatch = await user.matchPasswords(password);
+
+    if (!isMatch) {
+      res.status(404).json({
+        success: false,
+        error: "Invalid credentials",
+      });
+    }
+
+    res.status(201).json({
+      success: true,
+      token: "TOKENTOKEN",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
 };
 
 exports.forgotPassword = (req, res, next) => {
