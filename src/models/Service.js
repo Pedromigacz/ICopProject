1216323b9da691ca6c73a35b5e29b23a99ba8776
Pedro H G Migacz <pedromigacz@gmail.com>
2021-06-17 15:32:00 -0300

    add ServiceSchema

---
+++ src/models/Service.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const ServiceSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "each service must have a name!"],
+  },
+  price: String,
+  brand: String,
+  date: String,
+  address: String,
+  description: String,
+  comments: String,
+  listOfImages: [String],
+  owner: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "Travel",
+    required: [true, "Each service must have a (travel) owner!"],
+  },
+});
+
+const Service = mongoose.model("Travel", ServiceSchema);
+
+module.exports = Service;
