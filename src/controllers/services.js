d1fb72f2ff584225f30c89107cb7e19bd29e46fd
Pedro H G Migacz <pedromigacz@gmail.com>
2021-06-21 20:42:45 -0300

    add post service logic

--- src/controllers/services.js
+++ src/controllers/services.js
@@ -14,6 +14,38 @@ exports.getPrivateService = async (req, res, next) => {
     data: service,
   });
 };
+
+exports.postService = async (req, res, next) => {
+  if (!req.body || !req.body.service) {
+    return next(new ErrorResponse("missing service", 400));
+  }
+
+  const service = req.body.service;
+
+  let savedService;
+
+  try {
+    savedService = await Service.create({
+      name: service.name,
+      price: service.price,
+      brand: service.brand,
+      date: service.date,
+      address: service.address,
+      description: service.description,
+      comments: service.comments,
+      owner: mongoose.Types.ObjectId(service.ownerId),
+    });
+  } catch (error) {
+    return next(error);
+  }
+
+  res.status(200).json({
+    success: true,
+    data: "service created successfully",
+    serviceId: savedService._id,
+  });
+};
+
   res.status(200).json({
     success: true,
     data: "You got access to some private data",
