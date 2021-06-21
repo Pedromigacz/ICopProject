2ff03a3a6ea7d854db91a731002694c0224959f0
Pedro H G Migacz <pedromigacz@gmail.com>
2021-06-21 20:43:14 -0300

    add services delete logic

--- src/controllers/services.js
+++ src/controllers/services.js
@@ -46,8 +46,29 @@ exports.postService = async (req, res, next) => {
   });
 };
 
+exports.putScervice = async (req, res, next) => {
   res.status(200).json({
     success: true,
     data: "You got access to some private data",
+  });
+};
+
+exports.deleteService = async (req, res, next) => {
+  if (!req.params.serviceId) {
+    return next(new ErrorResponse("missing service", 400));
+  }
+
+  try {
+    const service = await Service.findById(req.params.serviceId);
+
+    if (!service) return next(new ErrorResponse("Service not found", 404));
+
+    await service.remove();
+  } catch (error) {
+    return next(error);
+  }
+  res.status(200).json({
+    success: true,
+    data: "service deleted successfully",
   });
 };
