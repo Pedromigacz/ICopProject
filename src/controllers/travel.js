f4a8e7e2938c9974b6bcf89d00736d69cd0838db
Pedro H G Migacz <pedromigacz@gmail.com>
2021-06-21 00:01:56 -0300

    add updateTravel routes

--- src/controllers/travel.js
+++ src/controllers/travel.js
@@ -24,13 +24,13 @@ exports.postTravel = async (req, res, next) => {
   const travel = req.body.travel;
 
   try {
-  const savedTravel = await Travel.create({
-    name: travel.name,
-    location: travel.location,
-    date: travel.date,
-    comments: travel.comments,
+    const savedTravel = await Travel.create({
+      name: travel.name,
+      location: travel.location,
+      date: travel.date,
+      comments: travel.comments,
       owner: mongoose.Types.ObjectId(travel.ownerId),
-  });
+    });
   } catch (error) {
     return next(error);
   }
@@ -42,10 +42,38 @@ exports.postTravel = async (req, res, next) => {
   });
 };
 
-exports.putTravel = (req, res, next) => {
+exports.putTravel = async (req, res, next) => {
+  if (!req.body || !req.body.travel) {
+    return next(new ErrorResponse("missing travel", 400));
+  }
+  if (!req.params.travelId) {
+    return next(new ErrorResponse("missing travelId", 400));
+  }
+
+  try {
+    const travel = await Travel.findById(req.params.travelId);
+
+    if (!travel) {
+      return next(new ErrorResponse("travel not found", 400));
+    }
+
+    travel.name = req.body.travel.name ? req.body.travel.name : travel.name;
+    travel.location = req.body.travel.location
+      ? req.body.travel.location
+      : travel.location;
+    travel.date = req.body.travel.date ? req.body.travel.date : travel.date;
+    travel.comments = req.body.travel.comments
+      ? req.body.travel.comments
+      : travel.comments;
+
+    await travel.save();
+  } catch (error) {
+    return next(error);
+  }
+
   res.status(200).json({
     success: true,
-    data: "You got access to some private data",
+    data: "travel updated successfully",
   });
 };
 
