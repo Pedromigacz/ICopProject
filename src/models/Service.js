28b90b14af18336eb16e67d144899a620e3fedb4
Pedro H G Migacz <pedromigacz@gmail.com>
2021-06-22 01:38:25 -0300

    add services remove prehook

--- src/models/Service.js
+++ src/models/Service.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const ErrorResponse = require("../utils/errorResponse.js");
 
 const ServiceSchema = new mongoose.Schema({
   name: {
@@ -13,7 +14,7 @@ const ServiceSchema = new mongoose.Schema({
   comments: String,
   listOfImages: [String],
   owner: {
-    type: [mongoose.Schema.Types.ObjectId],
+    type: mongoose.Schema.Types.ObjectId,
     ref: "Travel",
     required: [true, "Each service must have a (travel) owner!"],
   },
@@ -34,6 +35,29 @@ ServiceSchema.pre("save", async function (next) {
   }
   next();
 });
+
+ServiceSchema.pre("remove", async function (next) {
+  try {
+    const travelThatOwn = await mongoose
+      .model("Travel")
+      .findByIdAndUpdate(this.owner._id, {
+        $pull: { listOfServices: this._id },
+      });
+
+    if (!travelThatOwn) {
+      return next(
+        new ErrorResponse(
+          "internal server erro, please, contact us and we'll solve this issue as soon as possible",
+          500
+        )
+      );
+    }
+  } catch (error) {
+    return next(error);
+  }
+  next();
+});
+
 const Service = mongoose.model("Service", ServiceSchema);
 
 module.exports = Service;
