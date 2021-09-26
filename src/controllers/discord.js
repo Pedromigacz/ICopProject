e84809c5030300a9b30541cd8793f5cf44459ce0
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2021-09-26 03:11:09 -0300

    fix unnecessary change

--- src/controllers/discord.js
+++ src/controllers/discord.js
@@ -14,7 +14,6 @@ const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-console.log(envs.botToken);
 client.login(envs.botToken);
 
 client.on("ready", () => {
